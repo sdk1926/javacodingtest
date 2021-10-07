fc6299672b379bf27a86d1d7058fb4499398d259
sdk <sdk1926@naver.com>
2021-10-07 13:26:10 +0900

    ADD: 11653 소인수분해

---
+++ src/boj/math2/Factorization.java
@@ -0,0 +1,18 @@
+package boj.math2;
+
+import java.util.Scanner;
+
+public class Factorization {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=2; i <= Math.sqrt(n); i++ ){
+            while (n % i ==0){
+                System.out.println(i);
+                n /= i;
+            }
+        }
+        if (n != 1) {System.out.println(n);}
+    }
+}
---
+++ src/boj/math2/Main.java
@@ -0,0 +1,9 @@
+package boj.math2;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
