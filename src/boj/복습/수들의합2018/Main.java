6028cc31145e6b13186764bc5753e48f75b3c25d
sdk <sdk1926@naver.com>
2021-10-18 12:59:31 +0900

    Add: 2003 수들의 합 2 복습

---
+++ src/boj/복습/수들의합2018/Main.java
@@ -0,0 +1,9 @@
+package boj.복습.수들의합2018;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/boj/복습/수들의합2_2003/Main.java
+++ src/boj/복습/수들의합2_2003/Main.java
@@ -1,8 +1,36 @@
 package boj.복습.수들의합2_2003;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int target = sc.nextInt();
+        int[] lists = new int[N];
+        for (int i =0;i<N;i++){
+            lists[i] = sc.nextInt();
+        }
 
+        int p1 = 0;
+        int p2 = 0;
+        int cnt = 0;
+        int answ = 0;
+        while (true) {
+            if (cnt > target) {
+                cnt -= lists[p1];
+                p1 += 1;
+            }
+            else if ( p2 >= N) break;
+            else {
+                cnt += lists[p2];
+                p2 += 1;
+            }
+            if (cnt == target) {
+                answ += 1;
+            }
+        }
+        System.out.println(answ);
     }
 
 }
