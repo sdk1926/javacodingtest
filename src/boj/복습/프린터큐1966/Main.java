d7000e73489e7ba39ec3deb1cb5a3331fdcaa620
sdk <sdk1926@naver.com>
2021-10-20 11:43:54 +0900

    Add: 11866 요세푸스

--- src/boj/복습/프린터큐1966/Main.java
+++ src/boj/복습/프린터큐1966/Main.java
@@ -1,10 +1,28 @@
 package boj.복습.프린터큐1966;
 
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt();
+        int index = sc.nextInt();
+        sb.append("<");
+        Queue<Integer> q = new LinkedList<>();
+        for (int i=1;i<N+1;i++) {
+            q.add(i);
+        }
+        while (q.size() > 1) {
+            for (int i=0;i<index-1;i++){
+                int a =q.poll();
+                q.add(a);
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        sb.append(q.poll()).append(">");
+        System.out.println(sb);
     }
 }
