d54d3f4ad4897fe692afe69431a2493a31590ddd
sdk <sdk1926@naver.com>
2021-10-22 14:49:24 +0900

    ADD: 11866 요세푸스 복습

--- src/boj/복습/요세푸스_11866/Main.java
+++ src/boj/복습/요세푸스_11866/Main.java
@@ -6,7 +6,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        Queue<Integer> array = new LinkedList<>();
+        for (int i=1;i<N+1;i++) {
+            array.add(i);
+        }
+        while (array.size() > 1) {
+            for (int i=1;i<M;i++){
+                int a = array.poll();
+                array.add(a);
+            }
+            sb.append(array.poll()+", ");
+        }
+        sb.append(array.poll()+">");
+        System.out.println(sb);
     }
 }
 
