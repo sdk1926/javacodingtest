cc88734f6af3198aa8bc0b2c46358c3f17801fa7
sdk <sdk1926@naver.com>
2021-10-26 16:23:47 +0900

    WIP 2188 통계학

---
+++ src/boj/구현/통계학2108/Main.java
@@ -0,0 +1,31 @@
+package boj.구현.통계학2108;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i=0;i<N;i++) {
+            array.add(sc.nextInt());
+        }
+        int sum =0;
+        for(int i:array) {
+            sum += i;
+        }
+        double M = (double)N;
+        double avreage = sum / M;
+        int index = N/2 ;
+        Collections.sort(array);
+        int[] list = new int[N];
+        int i = 0;
+        System.out.println(Math.round(avreage));
+        System.out.println(array.get(index));
+        System.out.println(Collections.max(array)-Collections.min(array));
+
+    }
+}
